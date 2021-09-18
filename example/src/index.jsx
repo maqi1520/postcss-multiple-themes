43067c5e621d34a1d396ead32cdd910e8853dddd
马琪斌 <maqi1520@163.com>
2021-09-18 10:08:06 +0800

    refactor(example): actions

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style";
-import App from "./App";
+import App from "./app";
 
 var mountNode = document.getElementById("root");
 ReactDOM.render(<App />, mountNode);
